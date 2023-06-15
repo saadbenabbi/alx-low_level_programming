d307a2d59d16f7ef9b4f5944ffa701eb30f2b88f
saadbenabbi <benabbi.saad@gmail.com>
2023-06-15 15:56:46 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * code 
  * Description: a C program that prints exactly Programming is like building  a multilingual puzzle, followed by a new line. Use the function puts
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
