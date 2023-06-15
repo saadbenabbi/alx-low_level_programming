d1e3176cb297563843876fcd570a320cb0e0b033
saadbenabbi <benabbi.saad@gmail.com>
2023-06-15 16:01:48 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
+
 #include <stdio.h>
+
 /**
- * main - Entry point 
- * Description: a C program that prints exactly Programming is like building  a multilingual puzzle, followed by a new line. Use the function puts
+ * main - Entry point
+ *
+ * Description: Uses the function "puts"
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
