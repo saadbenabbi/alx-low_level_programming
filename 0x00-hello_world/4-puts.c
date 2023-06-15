ce7d733d7beef796c72700731df33b0ef6f863d9
saadbenabbi <benabbi.saad@gmail.com>
2023-06-15 15:58:59 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-/*
- *
- * code 
+/**
+ * main - Entry point 
  * Description: a C program that prints exactly Programming is like building  a multilingual puzzle, followed by a new line. Use the function puts
  */
 int main(void)
