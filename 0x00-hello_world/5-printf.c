bedcfabf231f5a8691b82e027aec1a05cbe8e773
saadbenabbi <benabbi.saad@gmail.com>
2023-06-15 16:03:10 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
+
 #include <stdio.h>
-/*
+
+/**
+ * main - Entry point
  *
- * code 
+ * Description: Uses the function "printf"
+ * Return: 0 (Success)
  */
-int main()
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
