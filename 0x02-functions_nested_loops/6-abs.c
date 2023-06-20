6acf1b7194f56ea93ebf4c0871b2a258d2d31616
saadbenabbi <benabbi.saad@gmail.com>
2023-06-20 16:22:28 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Function abs
+ *
+ * @n: int
+ *
+ * Description: function check abs
+ *
+ * Return: n and -n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	return (n);
+}
