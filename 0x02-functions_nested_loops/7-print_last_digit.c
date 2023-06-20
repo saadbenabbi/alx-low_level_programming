8f130e0dfa71229929dd6dad0de39e21c7c81d59
saadbenabbi <benabbi.saad@gmail.com>
2023-06-20 16:29:30 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - Function last digit
+ *
+ * @n: int
+ *
+ * Description: function last digit
+ *
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		_putchar((-n % 10) + '0');
+		return (-n % 10);
+	}
+	_putchar((n % 10) + '0');
+	return (n % 10);
+}
