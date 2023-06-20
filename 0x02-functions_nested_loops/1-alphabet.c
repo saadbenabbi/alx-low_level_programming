c22a2d19570f9871e2b3d80bb91b712e999d66d0
saadbenabbi <benabbi.saad@gmail.com>
2023-06-20 15:33:55 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Prints "Alphbet"
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
