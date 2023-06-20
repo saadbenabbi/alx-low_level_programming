af6e0eafb51a1b4261d223d33804aba5ca14f1c5
saadbenabbi <benabbi.saad@gmail.com>
2023-06-20 16:41:38 -0700

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,8 +12,9 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
-		_putchar(((n * -1) % 10) + '0');
-		return ((n * -1) % 10);
+		n=-n;
+		_putchar((n % 10) + '0');
+		return (n % 10);
 	}
 	_putchar((n % 10) + '0');
 	return (n % 10);
